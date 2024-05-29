2a33b1d006bef85155f452f08a84cd614f20ef51
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-05-29 23:55:05 +0600

    Model Creation and Code Generation for Friends Model

---
+++ GEO Location Service/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GEO_Location_Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GEO_Location_Service.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+        }
+        public DbSet<Friends> Friends { get; set; }
+
+
+    }
+
+}
---
+++ GEO Location Service/Models/Friends.cs
@@ -0,0 +1,11 @@
+﻿namespace GEO_Location_Service.Models
+{
+    public class Friends
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public double Let { get; set; } 
+        public double Lon { get; set; }
+    }
+}
